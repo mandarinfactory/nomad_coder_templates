0fda1e0fc1d006c5b1a92775483900234177dfd2
mandarinfactory <hoojunee@gmail.com>
2022-08-30 15:30:01 +0900

    JS_practice-17

--- momentum/apps.js
+++ momentum/apps.js
@@ -1,12 +1,20 @@
-const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector("#login-form");
 // querySelector로 볼때는 class(.), id(#)를 꼭 표시해줘야한다. //
 // querySelector가 아닌 getElementById로도 사용 가능하다. 이때는 따로 표시할 필요 없다. //
 const loginInput = loginForm.querySelector("input");
 // loginForm 또한 HTML element이므로(<div class="login-form">을 element화한 이름이다.) document가 아닌 loginForm으로 검색이 가능하다. //
 // === 좀더 정밀 조사가 가능하다. //
+const identity = document.querySelector("#identity");
+const HIDDEN_CLASSNAME = "hidden";
+// 일반적으로 string으로만 되어있는 변수는 대문자로 표기하곤 한다. //
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    identity.innerText = `Hello ${username}`;
+    // `Hello ${username}`;은 원래 "Hello " + username;꼴인데 백틱(``)으로 바꾼거다. //
+    identity.classList.remove(HIDDEN_CLASSNAME);
+    // classList에 아직 hidden이 있고, CSS.display.none이 되어있으므로, remove function을 통해 .hidden을 지워준다. //
 };
 loginForm.addEventListener("submit",onLoginSubmit);
 // addEventListener를 쓸때에는 function인 onloginSubmit을 바로 실행시키는건 아니므로 -Submit()식으로 기록하지 않는다. //
