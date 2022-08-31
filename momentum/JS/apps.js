bfcc907ce9f74c41222e03bd3be77a70aef58621
mandarinfactory <hoojunee@gmail.com>
2022-09-01 08:59:05 +0900

    JS_practice-19

--- momentum/JS/apps.js
+++ momentum/JS/apps.js
@@ -18,8 +18,8 @@ function onLoginSubmit(event) {
     searchIdentity(username);
 };
 loginForm.addEventListener("submit",onLoginSubmit);
-// addEventListener를 쓸때에는 function인 onloginSubmit을 바로 실행시키는건 아니므로 -Submit()식으로 기록하지 않는다. //
-// form내 input이나, enter를 눌러서 suvmit을 할때마다 browser는 자동으로 새로고침 된다.(browserdefault) //
+// addEventListener를 쓸때에는 function인 onloginSubmit을 바로 실행시키는건 아니므로 onLoginSubmit()식으로 기록하지 않는다. //
+// form내 input이나, enter를 눌러서 submit을 할때마다 browser는 자동으로 새로고침 된다.(browserdefault) //
 // 이걸 preventDefault로 막아준다. //
 // function(){}꼴로 쓰면 event 정보를 얻을수 없지만, (event)식으로 공간을 만들면, argument가 생기고 JS가 발생한 event에 대한 정보를 준다. = infomation //
 // addEventListener로 submit을 통해 onLoginSubmit function이 실행되면 첫번째 argument인 event가 실행된다. //
@@ -38,4 +38,11 @@ if(savedUsername === null){
 } else {
     searchIdentity(savedUsername);
 };
-// 4.7 RECAP //
+// localStorage는 정보를 저장, 불러오기, 삭제가 가능한 browser가 가지고 있는 작은 DB같은 API //
+// RECAP //
+// 1. savedUsername이 null이라면, loginForm(#login-form)에서 .hidden을 지워서 form을 나오게 한다. //
+// 2. addEventListener를 통해 user가 argument를 submit 하면, onLoginSubmit function이 실행된다. //
+// 3. onLoginSubmit에서 submit을 하면 preventDefault를 통해 새로고침인 browserDefault가 중지되고, loginForm에 다시 .hidden이 추가되서 지워진다. //
+// 4. input된 value(user가 입력한 argument) = username으로 element화 했고, localStorage에 username이라는 keyname과 함께 저장된다. //
+// 5. 이제 else를 통해 searchIdentity function이 실행되고, innerText로 hello + "username"꼴로 browser에 나타난다. (이때 #identity의 .hidden이 삭제되면서 나타난다.) //
+// 6. savedUsername으로 인해 localStorage에 저장된 username이 Hello + "username"꼴로 계속 나타나게 된다. //
